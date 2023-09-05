45ea1fca0eee3ce4626060e48614c16a699f9522
Jason Nichols <jason@startupos.dev>
2023-09-05 11:40:53 -0400

    this is stupid

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup
 
-import os
-import datetime
+# import os
+# import datetime
 
 
-# Get the version from the git tag, and write to VERSION.
-ref = None
-if "GITHUB_REF" in os.environ:
-    ref = os.environ["GITHUB_REF"]
+# # Get the version from the git tag, and write to VERSION.
+# ref = None
+# if "GITHUB_REF" in os.environ:
+#     ref = os.environ["GITHUB_REF"]
 
-if ref and ref is not None and ref.startswith("refs/tags/"):
-    version = ref.replace("refs/tags/", "")
-else:
-    version = datetime.datetime.now().strftime("%Y.%m.%d%H%M%S")
+# if ref and ref is not None and ref.startswith("refs/tags/"):
+#     version = ref.replace("refs/tags/", "")
+# else:
+#     version = datetime.datetime.now().strftime("%Y.%m.%d%H%M%S")
 
-print(version)
+# print(version)
 
-requirements = []
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+# requirements = []
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
 
 setup(
-    name='pygments_tsx',
-    version=version,
+    # name='pygments_tsx',
+    # version=version,
     package_dir={"": "src"},
     packages=['pygments_tsx'],
     entry_points="""
@@ -30,5 +30,5 @@ setup(
         jsx=jsx:JsxLexer
         tsx=pygments_tsx.tsx:TypeScriptXLexer
     """,
-    install_requires=requirements
+    # install_requires=requirements
     )
